16d581a13018f9e10f4c1bf4752d38c530bca76c
E-S-dev <ebaaalrass@gmail.com>
2025-01-18 18:03:00 -0600

    Register Commit

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\JsonResponse;
+
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'company' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        try {
+
+            DB::beginTransaction();
+            $user = User::create([
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'company' => $request->company,
+                'email' => $request->email,
+                'password' => Hash::make($request->string('password')),
+            ]);
+
+            $user->assignRole('user');
+            event(new Registered($user));
+            $token = $user->createToken('authToken'.$user->name)->plainTextToken;
+
+            DB::Commit();
+            return response()->json([
+                'message' => 'User Registered successfullu. An Verification email sent to your email!',
+                'token' => $token,
+                'user' => $user,
+                'role' => $user->getRoleNames(),
+            ], 200);
+        }
+        catch(\Exeption $e){
+            DB::rollBack();
+            return response()->json([
+                'message' => 'An error occure While registration proccess, please try again later!'
+            ],500);
+        }
+        
+
+        
+
+        
+
+        return response()->noContent();
+    }
+}
